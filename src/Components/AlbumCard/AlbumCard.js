99f38df9105dbdf27900a8e38ae0917e4baa97d1
antaga04 <antaga04@gmail.com>
2023-05-10 20:05:58 +0200

    Fixes

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -1,13 +1,12 @@
-// import "./newCard.css";
-import "../gpt.css";
+import "./albumCard.css";
 
 export const AlbumCard = (album) => {
   return `
     <article id="${album.id}" class="album-card">
       <img class="sub" src=${album.cover} alt='${album.name}' />
       <img draggable="false"class="super" src="https://res.cloudinary.com/drsfru9lj/image/upload/v1681747699/Rock%20Shop/tl_izzt88.webp" alt="cover">
-      <h3>${album.name}</h3>
-      <h5>${album.artist}</h5>
+      <h3 style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${album.name}</h3>
+      <h5 style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">${album.artist}</h5>
       <p>${album.price}€</p>
       <div class="buy-btn--container">
         <button id="add-to-cart" data-album-id="${album.id}">Add to Cart</button>
--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -8,15 +8,23 @@ export const Footer = () => {
       </div>
       <div>
         <ul class="flex social-icons">
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener"><i class="fa-brands fa-twitter"></i></a></li>
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener"><i class="fa-brands fa-linkedin-in"></i></a></li>
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener"><i class="fa-brands fa-facebook"></i></a></li>
-          <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener"><i class="fa-brands fa-instagram"></i></a></li>
+          <li>
+            <a href="https://github.com/antaga04" target="_blank" rel="noopener">
+              <i class="fa-brands fa-github"></i></a></li>
+          <li>
+            <a href="https://www.linkedin.com/in/adrian-anta-gil/" target="_blank" rel="noopener">
+              <i class="fa-brands fa-linkedin-in"></i></a></li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener">
+              <i class="fa-brands fa-facebook"></i></a></li>
+          <li>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank" rel="noopener">
+              <i class="fa-brands fa-instagram"></i></a></li>
         </ul>
       </div>
       <div class="right-footer">
         <p>POWERED BY ANTA</p>
-        <a href="#"><img src="https://res.cloudinary.com/drsfru9lj/image/upload/v1681568524/Rock%20Shop/solid-sounds-logo_vwl7d8.png" alt="Logo"></a>
+        <a href="/"><img src="https://res.cloudinary.com/drsfru9lj/image/upload/v1681568524/Rock%20Shop/solid-sounds-logo_vwl7d8.png" alt="Logo"></a>
       </div>
     </footer>
   `;
